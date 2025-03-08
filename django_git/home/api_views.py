6dc7dbd868edf4057af9948ae459daf123a00b7b
amin <aminmosavi4433@gmail.com>
2025-03-08 15:49:46 +0330

    add pagination to drf

--- django_git/home/api_views.py
+++ django_git/home/api_views.py
@@ -3,7 +3,7 @@ from .models import *
 from rest_framework.generics import ListCreateAPIView
 from .serializers import ProductSerializer
 from drf_spectacular.utils import extend_schema
-
+from rest_framework.pagination import LimitOffsetPagination
 
 @extend_schema(
     request=ProductSerializer,
@@ -13,4 +13,5 @@ from drf_spectacular.utils import extend_schema
 class ProductGenericApiView(ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    pagination_class = LimitOffsetPagination
 
