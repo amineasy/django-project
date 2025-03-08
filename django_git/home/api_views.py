35186156a9853776393d85390346ce9f2a185048
amin <aminmosavi4433@gmail.com>
2025-03-08 17:45:48 +0330

    complete all example of api for product model

--- django_git/home/api_views.py
+++ django_git/home/api_views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 from .models import *
-from rest_framework.generics import ListCreateAPIView
 from .serializers import ProductSerializer, MusicSerializer
 from drf_spectacular.utils import extend_schema
 from rest_framework.pagination import LimitOffsetPagination
+from rest_framework import generics, mixins, viewsets
 
 
 @extend_schema(
@@ -11,12 +11,61 @@ from rest_framework.pagination import LimitOffsetPagination
     responses={201: ProductSerializer},
     description="شاهکار",
 )
-class ProductGenericApiView(ListCreateAPIView):
+class ProductGenericApiView(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     pagination_class = LimitOffsetPagination
 
 
-class MusicGenericApiView(ListCreateAPIView):
+class MusicGenericApiView(generics.ListCreateAPIView):
     queryset = Music.objects.all()
     serializer_class = MusicSerializer
+
+
+#______________________________________________________________________________
+
+
+class ProductMixinApiView(
+    mixins.ListModelMixin,
+    mixins.CreateModelMixin,
+    mixins.RetrieveModelMixin,
+    mixins.UpdateModelMixin,
+    mixins.DestroyModelMixin,
+    generics.GenericAPIView
+):
+    queryset = Product.objects.all().order_by('id')
+    serializer_class = ProductSerializer
+
+    def get(self, request, *args, **kwargs):
+        if kwargs.get('pk'):  # بررسی اینکه pk مقدار داشته باشد
+            return self.retrieve(request, *args, **kwargs)
+        return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
+
+
+#_________________________________________________________________________
+
+class ProductGenericApiView(generics.ListCreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+
+class ProductDetailApiView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+
+#______________________________
+
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
--- django_git/home/urls.py
+++ django_git/home/urls.py
@@ -1,7 +1,10 @@
-from django.urls import path
+from django.urls import path,include
 from . import views
 from . import api_views
+from rest_framework.routers import DefaultRouter
 
+router = DefaultRouter()
+router.register('', api_views.ProductViewSet)
 
 app_name = 'home'
 
@@ -9,4 +12,12 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('product_gen',api_views.ProductGenericApiView.as_view(), name='product_gen'),
     path('music-gen',api_views.MusicGenericApiView.as_view(), name='music_gen'),
+    #____________
+    path('productmixin/', api_views.ProductMixinApiView.as_view(), name='product-list-create'),
+    path('productmixin/<int:pk>/', api_views.ProductMixinApiView.as_view(), name='product-mixin-detail'),
+    #____________
+    path('productgeneric/', api_views.ProductGenericApiView.as_view(), name='product-generic'),
+    path('productgeneric/<int:pk>', api_views.ProductDetailApiView.as_view(), name='product-generic'),
+    path('viewsets_product/',include(router.urls)),
+
 ]
