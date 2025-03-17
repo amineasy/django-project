dca5c4910971117cd853dd3d70cd0be4e759bbb5
amin <aminmosavi4433@gmail.com>
2025-03-17 10:35:03 +0330

    some change

--- django_git/django_git/settings.py
+++ django_git/django_git/settings.py
@@ -74,12 +74,34 @@ WSGI_APPLICATION = 'django_git.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "django_easy",
+        "USER": "postgres",
+        "PASSWORD": "aeduwnv20",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
+
+
+
+
+
+
+
+
+
+
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
